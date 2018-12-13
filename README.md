bf5562adeb46ac6ccd2f5b9d4824079610a6b632
Riley Butterfield <riley@scripted.org>
2018-12-13 23:11:31 +0000

    day 1 changes

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Make sure to check off your items as you complete them. [ ] => [x]
 - [ ] Add, commit, and push your changes
 - [x] Make your site live on gh-pages
 ### index.html
-- [ ] Update the HTML to reflect your three questions. Expected answers to your new questions should be placeholders in your input box.
+- [X] Update the HTML to reflect your three questions. Expected answers to your new questions should be placeholders in your input box.
 ### JS
 - [ ] Using conditional statements, declare a function for each of your questions that returns a number based on the user's input. 
 ### Wrap
--- js/script.js
+++ js/script.js
@@ -2,13 +2,68 @@
 
 $(document).ready(function() {
     $("button").click(function() {
-        var name = $("#name").val();
-        var techPlacement = "TBD";
+        var age = $("#age").val();
+        var cheeseSnack = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
-        var totalScore;
+        var cnStaff = $("#question3").val().length;
+        var totalScore = 0
+        function ageBox (age) {
+            age = parseInt(age)
+            if (age <= 16) {
+                return 1
+            }
+            else if (age <= 20) {
+                return 2  
+            }
+            else if (age <= 27) {
+                return 3   
+            }
+            else {
+                return 4
+            }
+        }
+        function cheeseBox (cheese) {
+            if (cheese === "cheddar" || cheese === "Cheddar") {
+                return 1
+            }
+            else if (cheese === "swiss" || cheese === "Swiss") {
+                return 2  
+            }
+            else if (cheese === "gouda" || cheese === "Gouda") {
+                return 3   
+            }
+            else {
+                return 4
+            }
+        }
+        function lunchBox (lunch) {
+            if (lunch === "sandwich" || lunch === "Sandwich") {
+                return 1
+            }
+            else if (lunch === "soup" || lunch === "Soup") {
+                return 2  
+            }
+            else {
+                return 3
+            }
+        }
+        function staffBox (cnStaff) {
+            if (cnStaff <= 4) {
+                return 1
+            }
+            else if (cnStaff <= 6) {
+                return 2  
+            }
+            else if (cnStaff <= 8) {
+                return 3   
+            }
+            else {
+                return 4
+            }
+        }
+        console.log(cheeseBox())
     });
 
 });
